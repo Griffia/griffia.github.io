4dbc4db9ed22421440eeaa9b34ea795585e3cbce
Griffia <100473677+Griffia@users.noreply.github.com>
2025-06-20 15:36:06 +0100

    Copy Contact Pop Up
    Added a feature to copy the contact details for church information

--- scripts/eventListners.js
+++ scripts/eventListners.js
@@ -20,11 +20,11 @@ const hideContactPopUp = () => {
 
 contactUs.addEventListener("mouseover", () => {
     hideContactPopUp();
-})
+});
 
 contactUsPopUp.addEventListener("mouseleave", (e) => {
     hideContactPopUp();
-})
+});
 
 const copyToClipBoard = () => {
     const getInfo = document.getElementById("copy-info");
@@ -32,12 +32,31 @@ const copyToClipBoard = () => {
 }
 
 const contactInfo = document.getElementsByClassName("contact-info");
-// console.log(contactInfo);
+const contactInfoCopyPopUp = document.getElementById("contact-info-copy-pop-up");
+const copyText = document.getElementById("copy-text");
 
 for (let i = 0; i <contactInfo.length; i++) {
     contactInfo[i].addEventListener("click", (e) => {
-        console.log("clicked");
-        console.log(e.target.innerText);
+        // console.log("clicked");
+        // console.log(e.target.innerText);
         navigator.clipboard.writeText(e.target.innerText);
-    })
+        copyText.innerText = "Copied!";
+        setTimeout(revertCopyText, 1500)
+    });
+
+    contactInfo[i].addEventListener("mouseover", (e) => {
+        contactInfoCopyPopUp.style.top = e.clientY - 50 + "px";
+        contactInfoCopyPopUp.style.left = e.clientX - 100 + "px";
+        contactInfoCopyPopUp.classList.toggle("hidden");
+    });
+
+    contactInfo[i].addEventListener("mouseleave", (e) => {
+
+        contactInfoCopyPopUp.classList.toggle("hidden");
+    });
+}
+
+
+const revertCopyText = () => {
+    copyText.innerText = "Click to Copy";
 }
